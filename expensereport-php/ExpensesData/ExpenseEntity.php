c7aa3b1516c3763c7168ebba80718db360c1c132
Mostafa Asker <mostafasalahaskar@gmail.com>
2026-01-03 17:03:36 +0200

    Expense class in seperated file

---
+++ expensereport-php/ExpensesData/ExpenseEntity.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Expense
+{
+    public $type;
+    public $amount;
+    function __construct($type, $amount) {
+        $this->type = $type;
+        $this->amount = $amount;
+    }
+}
