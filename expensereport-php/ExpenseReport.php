73ada0a18bfe78d0bf89fc1fd64e618e16f20beb
Mostafa Asker <mostafasalahaskar@gmail.com>
2026-01-03 16:16:48 +0200

    em

--- expensereport-php/ExpenseReport.php
+++ expensereport-php/ExpenseReport.php
@@ -29,18 +29,22 @@ class ExpenseReport
             if ($expense->type == ExpenseType::DINNER || $expense->type == ExpenseType::BREAKFAST) {
                 $mealExpenses += $expense->amount;
             }
-            $expenseName = "";
-            switch ($expense->type) {
-                case ExpenseType::DINNER: $expenseName = "Dinner"; break;
-                case ExpenseType::BREAKFAST: $expenseName = "Breakfast"; break;
-                case ExpenseType::CAR_RENTAL: $expenseName = "Car Rental"; break;
-            }
-
+            $expenseName = $this->getExpenseName($expense->type);
+        
             $mealOverExpensesMarker = $expense->type == ExpenseType::DINNER && $expense->amount > 5000 || $expense->type == ExpenseType::BREAKFAST && $expense->amount > 1000 ? "X" : " ";
             print($expenseName . "\t" . $expense->amount . "\t" . $mealOverExpensesMarker . "\n");
             $total += $expense->amount;
         }
         print("Meal Expenses: " . $mealExpenses . "\n");
         print("Total Expenses: " . $total . "\n");
+    }
+
+    public function getExpenseName($expenseType)
+    {
+        switch ($expenseType) {
+            case ExpenseType::DINNER: return "Dinner";
+            case ExpenseType::BREAKFAST: return "Breakfast";;
+            case ExpenseType::CAR_RENTAL: return "Car Rental"; ;
+        }
     }
 }
