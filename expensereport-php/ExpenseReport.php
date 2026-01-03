d9d194ff9f44e0c16c08b33ebeae4412361d3565
Mostafa Asker <mostafasalahaskar@gmail.com>
2026-01-03 16:43:19 +0200

    ExpenseData class

--- expensereport-php/ExpenseReport.php
+++ expensereport-php/ExpenseReport.php
@@ -24,7 +24,12 @@ class ExpensesData
 
 class ExpenseData 
 {
-
+    public function __construct (
+        public $type,
+        public $amount, 
+        public $name,
+        public $marker
+    ) {}
 }
 
 class ExpenseReport 
