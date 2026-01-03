c5def16ad092a3e6e54d08b8368e61672afea4c0
Mostafa Asker <mostafasalahaskar@gmail.com>
2026-01-03 17:07:15 +0200

    create transformer class

---
+++ expensereport-php/ExpensesData/ExpenseTransformer.php
@@ -0,0 +1,51 @@
+<?php 
+
+class ExpenseTransformer
+{
+    public function transform($expenses)
+    {
+        $expensesItems = [];
+        $mealExpenses = 0;
+        $total = 0;
+        foreach ($expenses as $expense) {
+            $mealExpenses += $this->getMealExpenses($expense);
+            $total += $expense->amount;
+            
+            $expensesItems [] = new ExpenseData(
+                $expense->type,
+                $expense->amount,
+                $this->getExpenseName($expense->type),
+                $this->getMealOverExpensesMarker($expense)
+            );
+        }
+        return new ExpensesData($mealExpenses, $total, $expensesItems);
+    }
+
+    private function getExpenseName($expenseType)
+    {
+        switch ($expenseType) {
+            case ExpenseType::DINNER: return "Dinner";
+            case ExpenseType::BREAKFAST: return "Breakfast";
+            case ExpenseType::CAR_RENTAL: return "Car Rental"; ;
+        }
+    }
+
+    private function getMealExpenses($expense)
+    {
+        if ($expense->type == ExpenseType::DINNER || $expense->type == ExpenseType::BREAKFAST) {
+            return $expense->amount;
+        }
+    }
+
+    private function getMealOverExpensesMarker($expense)
+    {
+        if($expense->type == ExpenseType::DINNER && $expense->amount > 5000){
+            return "X";
+        }
+
+        if($expense->type == ExpenseType::BREAKFAST && $expense->amount > 1000){
+            return "X";
+        }   
+        return " ";
+    }
+}
