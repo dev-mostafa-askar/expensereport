48f0c477729a5a8a8c0713095884580dd2a0fd97
Mostafa Asker <mostafasalahaskar@gmail.com>
2026-01-03 16:21:05 +0200

    xm

--- expensereport-php/ExpenseReport.php
+++ expensereport-php/ExpenseReport.php
@@ -50,4 +50,9 @@ class ExpenseReport
             return $expense->amount;
         }
     }
+
+    private function getMealOverExpensesMarker($expense)
+    {
+        $mealOverExpensesMarker = $expense->type == ExpenseType::DINNER && $expense->amount > 5000 || $expense->type == ExpenseType::BREAKFAST && $expense->amount > 1000 ? "X" : " ";
+    }
 }
